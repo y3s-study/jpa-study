df3d193034cb0e1f8321107e0b902b2398ff6797
Young Jun Park <june0313@gmail.com>
2021-07-05 20:01:37 +0900

    JPA Entity Event

--- jpa-start/src/main/java/jpabook/start/JpaDirtyCheckingMain.java
+++ jpa-start/src/main/java/jpabook/start/JpaDirtyCheckingMain.java
@@ -9,9 +9,15 @@ public class JpaDirtyCheckingMain {
             member.setAge(10);
 
             em.persist(member);
+            em.flush();
+            em.clear();
 
             Member findMember = em.find(Member.class, member.getId());
             findMember.setAge(200);
+
+            em.flush();
+
+            em.remove(findMember);
         });
         JpaTemplate.close();
     }
--- jpa-start/src/main/java/jpabook/start/Member.java
+++ jpa-start/src/main/java/jpabook/start/Member.java
@@ -1,12 +1,10 @@
 package jpabook.start;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +14,7 @@ import javax.persistence.Table;
 @Setter
 @Getter
 @DynamicUpdate
+@Slf4j
 public class Member {
     @Id
     @Column(name = "ID")
@@ -25,4 +24,44 @@ public class Member {
     private String username;
 
     private Integer age;
+
+    @PrePersist
+    public void prePersist() {
+        // DB에 저장되기 전 호출됨
+        log.info("### prePersist - " + this);
+    }
+
+    @PostPersist
+    public void postPersist() {
+        // DB에 저장된 후 호출됨
+        log.info("### postPersist - " + this);
+    }
+
+    @PostLoad
+    public void postLoad() {
+        // DB에서 조회 후 호출됨. 영속성 컨텍스트에서 바로 조회되는 경우 호출되지 않음
+        log.info("### postLoad - " + this);
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        // DB update query 실행 전 호출됨
+        log.info("### preUpdate - " + this);
+    }
+
+    @PostUpdate
+    public void postUpdate() {
+        // DB update query 실행 후 호출됨
+        log.info("### postUpdate - " + this);
+    }
+
+    @PreRemove
+    public void preRemove() {
+        log.info("### preRemove - " + this);
+    }
+
+    @PostRemove
+    public void postRemove() {
+        log.info("### postRemove - " + this);
+    }
 }
