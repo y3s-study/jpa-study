045130a3aa7c5d31365636bc41786ead5a6f31c9
songseongbin <ppsosqq@gmail.com>
2021-07-17 14:29:22 +0900

    4. 엔티티 매핑

--- sb-jpa-start/src/main/java/jpabook/start/Member.java
+++ sb-jpa-start/src/main/java/jpabook/start/Member.java
@@ -1,21 +1,44 @@
 package jpabook.start;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="MEMBER")
+@Table(name="MEMBER", uniqueConstraints = {@UniqueConstraint(
+    name = "NAME_AGE_UNIQUE",
+    columnNames = {"NAME", "AGE"}
+)})
 public class Member {
     @Id
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10)
     private String username;
 
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public String getId() {
         return id;
