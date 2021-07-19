9004e48166f79b71ab208dd159afc3ef9c0458da
songseongbin <ppsosqq@gmail.com>
2021-07-19 22:24:15 +0900

    4. 엔티티 매핑
    - 실전예제

---
+++ sb-jpa-start/src/main/java/jpabook/ch4/Order.java
@@ -0,0 +1,61 @@
+package jpabook.ch4;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Order {
+	@Id
+	@GeneratedValue
+	@Column(name = "ORDER_ID")
+	private Long id;
+
+	@Column(name = "MEMBER_ID")
+	private Long memberId;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date orderDate;
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+}
---
+++ sb-jpa-start/src/main/java/jpabook/ch4/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpabook.ch4;
+
+public enum OrderStatus {
+	ORDER, CANCEL
+}
