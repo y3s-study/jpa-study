90e7cd31209c69cdc7e151fd21dac8b7e5221276
songseongbin <ppsosqq@gmail.com>
2021-08-22 11:02:27 +0900

    9. 값 타입 - 임베디드 타입과 연관관계
    - 엔티티 생성 확인

--- sb-jpa-start/src/main/java/jpabook/ch9/embedded/association/Member.java
+++ sb-jpa-start/src/main/java/jpabook/ch9/embedded/association/Member.java
@@ -2,11 +2,16 @@ package jpabook.ch9.embedded.association;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Member {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+
 	@Embedded
 	private Address address;
 	@Embedded
