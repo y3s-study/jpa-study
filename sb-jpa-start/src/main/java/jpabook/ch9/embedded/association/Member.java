2b519a56150a526a3abd493522947cdcf3110fe2
songseongbin <ppsosqq@gmail.com>
2021-08-22 11:31:10 +0900

    9. 값 타입 - 속성 재정의

--- sb-jpa-start/src/main/java/jpabook/ch9/embedded/association/Member.java
+++ sb-jpa-start/src/main/java/jpabook/ch9/embedded/association/Member.java
@@ -1,5 +1,8 @@
 package jpabook.ch9.embedded.association;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +16,18 @@ public class Member {
 	private String name;
 
 	@Embedded
-	private Address address;
+	private Address homeAddress;
+
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "city", column = @Column(name = "COMPANY_CITY")),
+		@AttributeOverride(name = "street", column = @Column(name = "COMPANY_STREET")),
+		@AttributeOverride(name = "state", column = @Column(name = "COMPANY_STATE")),
+		@AttributeOverride(name = "zipcode.zip", column = @Column(name = "COMPANY_ZIPCODE_ZIP")),
+		@AttributeOverride(name = "zipcode.plusFour", column = @Column(name = "COMPANY_ZIPCODE_PLUS_FOUR"))
+	})
+	private Address companyAddress;
+
 	@Embedded
 	private PhoneNumber phoneNumber;
 }
