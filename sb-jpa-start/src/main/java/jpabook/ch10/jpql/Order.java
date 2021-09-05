4ab2cf6ded142fb3f5562d7efd23d0b314dca904
songseongbin <ppsosqq@gmail.com>
2021-09-05 17:52:49 +0900

    10. 객체지향 쿼리 언어 - 객체지향 쿼리 언어2
    - mapping-file 위치 수정
    - persistence.xml 엔티티 수정

--- sb-jpa-start/src/main/java/jpabook/ch10/jpql/Order.java
+++ sb-jpa-start/src/main/java/jpabook/ch10/jpql/Order.java
@@ -1,4 +1,12 @@
 package jpabook.ch10.jpql;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Order {
+	@Id
+	@GeneratedValue
+	private Long id;
 }
--- sb-jpa-start/src/main/java/jpabook/ch10/jpql/Team.java
+++ sb-jpa-start/src/main/java/jpabook/ch10/jpql/Team.java
@@ -3,13 +3,22 @@ package jpabook.ch10.jpql;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 
 @Getter
 @Entity
 public class Team {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
 	private String name;
 
+	@OneToMany
 	private List<Member> members;
 }
