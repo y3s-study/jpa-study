ea80d1fc985d576cadbb20c07a8a1b2862d767f8
songseongbin <ppsosqq@gmail.com>
2021-10-13 20:21:23 +0900

    14.4 엔티티 그래프
    - 동적 엔티티 그래프

--- sb-jpa-start/src/test/java/jpabook/ch14/subgraph/SubgraphTest.java
+++ sb-jpa-start/src/test/java/jpabook/ch14/subgraph/SubgraphTest.java
@@ -3,6 +3,9 @@ package jpabook.ch14.subgraph;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.persistence.EntityGraph;
+import javax.persistence.Subgraph;
+
 import org.junit.jupiter.api.Test;
 
 import jpabook.JpaTest;
@@ -23,5 +26,29 @@ public class SubgraphTest extends JpaTest {
 			.setParameter("orderId", 1L)
 			.setHint("javax.persistence.fetchgraph", em.getEntityGraph("Order.withAll"))
 			.getResultList();
+	}
+
+	@Test
+	void 동적_엔티티_graph() {
+		EntityGraph<Order> graph = em.createEntityGraph(Order.class);
+		graph.addAttributeNodes("member");
+
+		HashMap<String, Object> hints = new HashMap<>();
+		hints.put("javax.persistence.fetchgraph", graph);
+
+		em.find(Order.class, 1L, hints);
+	}
+
+	@Test
+	void 동적_엔티티_subgraph() {
+		EntityGraph<Order> graph = em.createEntityGraph(Order.class);
+		graph.addAttributeNodes("member");
+		Subgraph<OrderItem> orderItems = graph.addSubgraph("orderItems");
+		orderItems.addAttributeNodes("item");
+
+		HashMap<String, Object> hints = new HashMap<>();
+		hints.put("javax.persistence.fetchgraph", graph);
+
+		Order order = em.find(Order.class, 1L, hints);
 	}
 }
