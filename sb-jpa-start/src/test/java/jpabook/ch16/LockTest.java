87cf65ec673276ec002f19dfebdc03800f08879a
songseongbin <ppsosqq@gmail.com>
2021-10-24 12:21:05 +0900

    16.1.5 JPA 낙관적 락

--- sb-jpa-start/src/test/java/jpabook/ch16/LockTest.java
+++ sb-jpa-start/src/test/java/jpabook/ch16/LockTest.java
@@ -1,12 +1,13 @@
 package jpabook.ch16;
 
 import javax.persistence.EntityTransaction;
+import javax.persistence.LockModeType;
 
 import jpabook.JpaTest;
 
 public class LockTest extends JpaTest {
 	void 버전_관리_사용() {
-		// 트랜잭션 1조회 title="제목A", verison=1
+		// 트랜잭션 1조회 title="제목A", version=1
 		EntityTransaction tx = em.getTransaction();
 		Board board = em.find(Board.class, 1L);
 
@@ -16,5 +17,27 @@ public class LockTest extends JpaTest {
 
 		em.persist(board);
 		tx.commit(); // 예외 발생, 데이터베이스 version=2, 엔티티 version=1
+	}
+
+	void OPTIMISTIC() {
+		// 트랜잭션 1조회 title="제목A", version=1
+		EntityTransaction tx = em.getTransaction();
+		Board board = em.find(Board.class, 1L);
+
+		// 트랜잭션2에서 해당 게시물을 수정해서 title="제목C", version=2로 증가
+
+		//트랜잭션1 커밋 시점에 버전 정보 검증, 예외 발생
+		//(데이터베이스 version=2, 엔티티 version=1)
+		tx.commit();
+	}
+
+	void OPTIMISTIC_FORCE_INCREMENT() {
+		// 트랜잭션1 조회 title="제목A", version=1
+		EntityTransaction tx = em.getTransaction();
+		Board board = em.find(Board.class, 1L, LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+
+		//트랜잭션1 커밋 시점에 버전 강제 증가
+		tx.commit();
+
 	}
 }
